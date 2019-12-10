529b98c538286aaf767c74c484588d0d6b020bff
Jeton Thaçi <jetonthaci@outlook.com>
2019-12-10 11:59:16 +0100

    add currentUser and get rooms

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,8 +3,40 @@ import Chatkit from '@pusher/chatkit-client';
 import { tokenUrl, instanceLocator } from '../../config';
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [roomId, setRoomId] = useState(null);
+  const [joinableRooms, setJoinableRooms] = useState([]);
+  const [joinedRooms, setJoinedRooms] = useState([]);
 
+
+  const getRooms = () => {
+    currentUser.getJoinableRooms()
+      .then((rooms) => {
+        setJoinableRooms(rooms);
+        setJoinedRooms(currentUser.rooms);
+      })
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: 'jeton',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: tokenUrl,
+      }),
+    });
+
+    chatManager.connect()
+      .then((user) => {
+        setCurrentUser(user);
+        getRooms();
+      })
+      .catch((err) => err);
+  }, [roomId]);
+
+  console.log(joinableRooms);
+  console.log(joinedRooms);
 
   return (
     <div className="App">
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c7ab725a-a1f0-4d9e-aaf6-5bb0400dd828/token";
-const instanceLocator = "v1:us1:c7ab725a-a1f0-4d9e-aaf6-5bb0400dd828";
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c7ab725a-a1f0-4d9e-aaf6-5bb0400dd828/token';
+const instanceLocator = 'v1:us1:c7ab725a-a1f0-4d9e-aaf6-5bb0400dd828';
 
-export { tokenUrl, instanceLocator }
+export { tokenUrl, instanceLocator };
